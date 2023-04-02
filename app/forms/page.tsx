640a0746cecccf2f481a8c1dfb248ef24073d756
Alejandro Asensio <alejandro.asensio.10@gmail.com>
2023-04-02 05:48:52 +0200

    Use Roboto Flex from next/font/google

--- app/forms/page.tsx
+++ app/forms/page.tsx
@@ -1,22 +1,14 @@
 'use client'
 
-// import { Roboto_Flex } from 'next/font/google'
 import { Box, ThemeProvider } from '@mui/material'
 import AppBar from '@/app/components/AppBar'
 import NewFormSection from '@/app/components/NewFormSection'
 import { theme } from '@/app/theme'
 
-// const roboto = Roboto_Flex({
-//   weight: ['400', '700'],
-//   subsets: ['latin'],
-// })
-
 export default function Forms() {
   return (
     <ThemeProvider theme={theme}>
-      <main
-      // className={roboto.className}
-      >
+      <main>
         <Box sx={{ color: 'grey.700' }}>
           <AppBar />
           <NewFormSection />
--- app/theme.ts
+++ app/theme.ts
@@ -1,6 +1,15 @@
+import { Roboto_Flex } from 'next/font/google'
 import { createTheme } from '@mui/material'
 
+const roboto = Roboto_Flex({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+})
+
 export const theme = createTheme({
+  typography: {
+    fontFamily: [roboto.style.fontFamily, 'sans-serif'].join(','),
+  },
   components: {
     MuiButton: {
       styleOverrides: {
