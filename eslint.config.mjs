1f3cf3280b4640f93beb31dacb7509b22ed34cd0
Alejandro Asensio <alejandro.asensio@trustyou.net>
2026-07-05 22:57:02 +0200

    Add react compiler

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig, globalIgnores } from 'eslint/config'
 import nextVitals from 'eslint-config-next/core-web-vitals'
+import reactCompiler from 'eslint-plugin-react-compiler'
  
 const eslintConfig = defineConfig([
   ...nextVitals,
@@ -11,6 +12,14 @@ const eslintConfig = defineConfig([
     'build/**',
     'next-env.d.ts',
   ]),
+  {
+    plugins: {
+      'react-compiler': reactCompiler,
+    },
+    rules: {
+      'react-compiler/react-compiler': 'error',
+    },
+  },
 ])
  
 export default eslintConfig
--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  reactCompiler: true,
 }
 
 module.exports = nextConfig
